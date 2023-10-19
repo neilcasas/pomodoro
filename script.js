de2844e8cf196b894c07bc32edd37ace43247cbb
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-19 11:08:08 +0800

    Added updateTimer and setCustomTimer function

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 let time = 600; // Set the initial time to 10 minutes (600 seconds)
 const countdown = document.getElementById('countdown');
 
-function startTimer(){
-    // Update the timer every second
-    const timer = setInterval(function() {
-        if (time > 0) {
-            const minutes = Math.floor(time / 60);
-            const seconds = time % 60;
-            countdown.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-            time--;
-        } else {
-            clearInterval(timer); // Stop the timer when it reaches 0
-            countdown.textContent = "Time's up!";
-        }
-    }, 1000); // 1000 milliseconds = 1 second
+function startTimer() {
+    const inputMinutes = parseInt(document.getElementById('minutes').value);
+    if (!isNaN(inputMinutes) && inputMinutes >= 1) {
+        time = inputMinutes * 60;
+        updateTimerDisplay();
+        const timer = setInterval(function() {
+            if (time > 0) {
+                time--;
+                updateTimerDisplay();
+            } else {
+                clearInterval(timer);
+                countdown.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
 }
 
-function pauseTimer(){
-    
+function updateTimerDisplay() {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    countdown.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
+function setCustomTimer() {
+    const inputMinutes = parseInt(document.getElementById('minutes').value);
+    if (!isNaN(inputMinutes) && inputMinutes >= 1) {
+        time = inputMinutes * 60;
+        updateTimerDisplay();
+    }
 }
