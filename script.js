01435f802385252db56d7b8aa05a256c9f491e66
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-19 12:09:38 +0800

    Fixed start timer issue where multiple timers play simultaneously

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ let time = 600; // Set the initial time to 10 minutes (600 seconds)
 const countdown = document.getElementById('countdown');
 const timerEnd = new Audio('timerend.mp3');
 let isPaused = true;
+const startButton = document.querySelector('.start-button');
+const adjustTimeContainer = document.querySelector('.adjust-time-container');
+
+let timerId; // Variable to store the timer ID
 
 function startTimer() {
+    startButton.style.display = 'none';
+    adjustTimeContainer.style.display = 'none';
+    if (timerId) {
+        // Clear the existing timer
+        clearInterval(timerId);
+    }
+    
     const inputMinutes = parseInt(document.getElementById('minutes').value);
     if (!isNaN(inputMinutes) && inputMinutes >= 1) {
         if (isPaused) {
+            // Remove start button once playing
+
             // Resume the timer
             isPaused = false;
         } else {
@@ -14,20 +27,21 @@ function startTimer() {
             updateTimerDisplay();
         }
 
-        const timer = setInterval(function() {
+        timerId = setInterval(function() {
             if (!isPaused) {
                 if (time > 0) {
                     time--;
                     updateTimerDisplay();
                 } else {
-                    clearInterval(timer);
+                    clearInterval(timerId); // Clear the timer
                     timerEnd.play();
+                    startButton.style.display = 'inline';
+                    adjustTimeContainer.style.display = 'inline';
                 }
             }
         }, 1000);
     }
 }
-
 
 function updateTimerDisplay() {
     const minutes = Math.floor(time / 60);
